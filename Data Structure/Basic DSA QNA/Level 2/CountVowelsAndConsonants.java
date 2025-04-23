7b5f7acb84f718b8605e6a3729fe4b6136fc39dd
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2025-04-23 13:03:42 +0530

    Added program to count vowels and consonants of a string

---
+++ Data Structure/Basic DSA QNA/Level 2/CountVowelsAndConsonants.java
@@ -0,0 +1,35 @@
+// Count vowels and consonants in a string.
+
+import java.util.Scanner;
+
+public class CountVowelsAndConsonants{
+    private boolean checkVowel(char ch){
+        return false;
+    }
+    public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+
+       System.out.println("Enter a string to count vowels and consonants: ");
+       String str = sc.nextLine();
+
+       int vowels = 0, consonants = 0;
+       for(char ch: str.toCharArray()){
+            if(Character.isLetter(ch)){
+                if("aeiou".indexOf(ch) != -1) vowels++;
+                else consonants++;
+            }
+       }
+       System.out.println("Total Vowels: " + vowels);
+       System.out.println("Total Consonants: " + consonants);
+    }
+}
+
+/* 
+OUTPUT:
+------
+Enter a string to count vowels and consonants: 
+hello i am learning java here
+Total Vowels: 11
+Total Consonants: 13
+
+*/
