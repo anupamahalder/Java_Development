ebb58097bb4e2ce4c23badbe539df42c1cfc3cd5
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2025-04-27 13:17:58 +0530

    Added HashSet class

---
+++ Collection Framework/Collection Framework Details/Set/HashSetClass.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetClass{
+    public static void main(String[] args) {
+        // Creating object of List interface
+        Set<Integer> set = new HashSet<>();
+
+        // Add elements to set
+        set.add(3);
+        set.add(4);
+        set.add(3);
+        set.add(8);
+        set.add(4);
+        set.add(3);
+        set.add(7);
+
+        // print set 
+        System.out.println(set); // [3, 4, 7, 8]
+
+        // remove element 
+        set.remove(4); // Direct element have to give here
+        System.out.println(set); // [3, 7, 8]
+
+        // remove all elements
+        set.clear();
+        System.out.println(set); // []
+    }
+}
